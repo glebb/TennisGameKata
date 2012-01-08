540c5e5fde8732963a2e556a8b62a6a427326c8c
Antti Niiles <antti.niiles@gmail.com>
2012-01-08 17:07:15 +0200

    refactored example, shorter and cleaner

--- example/tennisgame.py
+++ example/tennisgame.py
@@ -9,25 +9,43 @@ class TennisGame(object):
         return self.scoreLiterals[self.playerOneProgress] + " - " + self.scoreLiterals[self.playerTwoProgress]
         
     def playerOneScores(self):
-        if self.playerTwoProgress == self.scoreLiterals.index("adv"):
-            self.playerTwoProgress = self.scoreLiterals.index("40")
+        if self._otherPlayerLosesAdvantage(1):
             return
-        if self.playerOneProgress == self.scoreLiterals.index("40") and self.playerTwoProgress < self.scoreLiterals.index("30"):
-            self.playerOneProgress = self.scoreLiterals.index("wins")
-            self.playerTwoProgress = self.scoreLiterals.index("loses")    
+        elif self._playerWins(1):
+            return        
         else:
             self.playerOneProgress += 1
-        if self.playerOneProgress == self.scoreLiterals.index("wins"):
-            self.playerTwoProgress = self.scoreLiterals.index("loses")
     
     def playerTwoScores(self):
-        if self.playerOneProgress == self.scoreLiterals.index("adv"):
-            self.playerOneProgress = self.scoreLiterals.index("40")
+        if self._otherPlayerLosesAdvantage(2):
             return
-        if self.playerTwoProgress == self.scoreLiterals.index("40") and self.playerOneProgress < self.scoreLiterals.index("30"):
-            self.playerTwoProgress = self.scoreLiterals.index("wins")
-            self.playerOneProgress = self.scoreLiterals.index("loses")
+        elif self._playerWins(2):
+            return
         else:
-            self.playerTwoProgress += 1        
-        if self.playerTwoProgress == self.scoreLiterals.index("wins"):
-            self.playerOneProgress = self.scoreLiterals.index("loses")
+            self.playerTwoProgress += 1                
+        
+    def _otherPlayerLosesAdvantage(self, player):
+        if player == 1 and self.playerTwoProgress == self.scoreLiterals.index("adv"):
+            self.playerTwoProgress = self.scoreLiterals.index("40")
+            return True
+        if player == 2 and self.playerOneProgress == self.scoreLiterals.index("adv"):
+            self.playerOneProgress = self.scoreLiterals.index("40")
+            return True
+        return False
+
+    def _playerWins(self, player):
+        if player == 1:
+            if self.playerOneProgress == self.scoreLiterals.index("40") and self.playerTwoProgress < self.scoreLiterals.index("40") \
+            or self.playerOneProgress == self.scoreLiterals.index("adv"):
+                self.playerOneProgress = self.scoreLiterals.index("wins")
+                self.playerTwoProgress = self.scoreLiterals.index("loses")
+                return True
+        if player == 2:
+            if self.playerTwoProgress == self.scoreLiterals.index("40") and self.playerOneProgress < self.scoreLiterals.index("40") \
+            or self.playerTwoProgress == self.scoreLiterals.index("adv"):
+                self.playerTwoProgress = self.scoreLiterals.index("wins")
+                self.playerOneProgress = self.scoreLiterals.index("loses")
+                return True
+        return False
+
+
